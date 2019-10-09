731ad2ab262b2539291780737de1d48270508b11
S. Marie <sdhmariedesign@gmail.com>
2019-10-09 14:55:16 -0700

    Adds constructor for LinkedList class.

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -1,18 +1,19 @@
 //Exercises from Section 21 on Linked Lists 
 
-//Create node class with a constructor.
 class Node {
-    //Define the constructor, which is called with two properties - (data, next).
-    //In cases where next is left out, set next equal to null to show that it was intentially left out, (rather than a property returning as undefined).
     constructor(data, next = null) {
-        //Assign data property.
         this.data = data;
-        //Assign next property.
         this.next = next;
     }
 }
 
-class LinkedList { }
+//Create constructor for the LinkedList class, which only contains the head property.
+//Head should be set to null by default.
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+}
 
 module.exports = { Node, LinkedList };
 
