2f0ac2217a8d9f7d5d126993581d91e33ebd8860
S. Marie <sdhmariedesign@gmail.com>
2019-10-09 14:48:54 -0700

    First commit, adds node class.

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -1,9 +1,9 @@
 //Exercises from Section 21 on Linked Lists 
 
+//Create node class with a constructor.
 class Node {
-    //Define constructor function to initialize a new node.
-    //The constructor will be called with two properties - (data, next).
-    //In cases where next is left out, set next equal to null to show that it was intentially left out, rather than a property returning as undefined.
+    //Define the constructor, which is called with two properties - (data, next).
+    //In cases where next is left out, set next equal to null to show that it was intentially left out, (rather than a property returning as undefined).
     constructor(data, next = null) {
         //Assign data property.
         this.data = data;
