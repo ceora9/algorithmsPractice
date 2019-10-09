67f2d01aaf8d760985abbd43d620ebfc4b482d8f
S. Marie <sdhmariedesign@gmail.com>
2019-10-09 15:16:41 -0700

    Adds insert method.

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -7,12 +7,21 @@ class Node {
     }
 }
 
-//Create constructor for the LinkedList class, which only contains the head property.
-//Head should be set to null by default.
 class LinkedList {
     constructor() {
         this.head = null;
     }
+
+    //An insert method will initialze the linked list by creating a node out of data, and assign it to the 'head' property. 
+    insertFirst(data) {
+        //Passing 'this.head' inserts at the head, but the pointer also needs to be reset.
+        const node = new Node(data, this.head);
+        //To reset pointer to the head:
+        this.head = node;
+
+        //This method can be condensed into one line of code: 
+        //this.head = new Node (data, this.head);
+    };
 }
 
 module.exports = { Node, LinkedList };
