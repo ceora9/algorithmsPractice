d85201a52517bcafd7883b23a6389414cd86dfe7
S. Marie <sdhmariedesign@gmail.com>
2019-10-09 15:40:34 -0700

    Condenses code in insert method.

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -12,15 +12,8 @@ class LinkedList {
         this.head = null;
     }
 
-    //An insert method will initialze the linked list by creating a node out of data, and assign it to the 'head' property. 
     insertFirst(data) {
-        //Passing 'this.head' inserts at the head, but the pointer also needs to be reset.
-        const node = new Node(data, this.head);
-        //To reset pointer to the head:
-        this.head = node;
-
-        //This method can be condensed into one line of code: 
-        //this.head = new Node (data, this.head);
+        this.head = new Node(data, this.head); //Updated with condensed code.
     };
 }
 
