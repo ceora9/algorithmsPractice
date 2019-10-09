a2557339479f8363a09c7c774c756b7c682370f7
S. Marie <sdhmariedesign@gmail.com>
2019-10-09 16:25:33 -0700

    Adds method to return size of the linked list.

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -13,8 +13,24 @@ class LinkedList {
     }
 
     insertFirst(data) {
-        this.head = new Node(data, this.head); //Updated with condensed code.
+        this.head = new Node(data, this.head);
     };
+
+    //Create a method to iterate over the list and return an integer representing the number of nodes.
+    //Because the LinkedList class only points to the head, set up a counter variable and node variable equal to the head. 
+    size() {
+        let counter = 0;
+        //Reference directly to the head
+        let node = this.head;
+
+        //Set up a while loop to increment the counter at each iteration, using node.next, and reassign the node variable so it keeps looping until the value is null at the tail.
+
+        while (node) {
+            counter++;
+            node = node.next;
+        }
+        return counter;
+    }
 }
 
 module.exports = { Node, LinkedList };
